c7e86331e2787a7cd37a9111e8f8d72d8f8408be
Mateusz Hallala <matt@huncwot.com>
2023-04-26 00:48:25 +0200

    test js crypto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { styled } from './styles/stitches';
 import { flex } from './styles/utils';
 import Form from './components/Form/Form';
+import { useEffect } from 'react';
 
 function App() {
+    useEffect(() => {
+        console.log('Hello from App.tsx');
+        console.log(self.crypto.getRandomValues(new BigUint64Array(10)));
+        // const array = new Uint32Array(10);
+        // self.crypto.getRandomValues(array);
+
+        // console.log('Your lucky numbers:');
+        // for (const num of array) {
+        //     console.log(num);
+        // }
+    });
+
     return (
         <Main>
             <Form />
